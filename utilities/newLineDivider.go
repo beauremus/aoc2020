4ed23ad11224551c2512a25cc5ac7a500b1bdb8a
Beau Harrison <beau@fnal.gov>
2020-12-09 09:18:49 -0600

    Remove commented code.

--- utilities/newLineDivider.go
+++ utilities/newLineDivider.go
@@ -6,16 +6,7 @@ import (
 
 // NewLiveDivider takes a string of file contents and returns groups by blank newline
 func NewLiveDivider(input string) []string {
-	// var output []string
 	output := strings.Split(strings.ReplaceAll(input, "\n", " "), "  ")
-
-	// for _, value := range temp {
-	// 	fmt.Printf("%v\n", value)
-	// }
-
-	// fmt.Printf("%v\n", temp)
-
-	// output = append(output, "Not dead yet!")
 
 	return output
 }
